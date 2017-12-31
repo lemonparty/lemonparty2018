d59b356900bef50313898901ae2706619d472775
Michael P. Geraci <me@mgeraci.com>
2017-12-31 10:27:51 -0600

    inital shitty login, without hashing or styles

--- lemonparty2018.py
+++ lemonparty2018.py
@@ -1,13 +1,36 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, session, flash, request
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if session.get('logged_in'):
+        return render_template('home.html')
+    else:
+        return render_template('login.html', login_error=session.get('login_error'))
+
+
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'password':
+        session['logged_in'] = True
+        session['login_error'] = False
+    else:
+        session['login_error'] = True
+
+    return index()
+
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+
+    return index()
 
 
 if __name__ == '__main__':
+    app.secret_key = os.urandom(12)
     app.run(debug=True, host='0.0.0.0')
