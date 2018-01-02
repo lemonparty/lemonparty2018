e98e03bea924a79f3ed0ea4874af3f9d40c1e5d1
Michael P. Geraci <me@mgeraci.com>
2018-01-01 19:19:41 -0600

    fix wsgi import

--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,6 @@
-from lemonparty2018 import app as application
+# since the name of the project is the same as the name of the script, the
+# import here needs to be slightly more specific than the traditional
+# `from lemonparty2018 import app as application`
+
+from lemonparty2018 import lemonparty2018
+application = lemonparty2018.app
