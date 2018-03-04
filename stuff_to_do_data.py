c742aae24ba5aa3b2c41e70f4745880d256b3dba
Michael P. Geraci <me@mgeraci.com>
2018-03-04 17:26:47 -0500

    update stuff to do with more copy and better linebreaks

--- stuff_to_do_data.py
+++ stuff_to_do_data.py
@@ -8,52 +8,92 @@ STUFF_TO_DO = [
             {
                 'name': 'Canal Park',
                 'link': 'https://www.google.com/maps/place/Canal+Park,+Duluth,+MN+55802/@46.7835903,-92.1049547,15z/data=!3m1!4b1!4m5!3m4!1s0x52ae52eb33b311c7:0x30eb1913b30b1eb7!8m2!3d46.783164!4d-92.0950821?hl=en',
-                'description': 'A business district in downtown that has some of our favorite shops, like Duluth Pack and the Waters of Superior gallery.',
+                'description': '''
+                    A business district in downtown that has some of our
+                    favorite shops, like Duluth Pack and the Waters of Superior
+                    gallery.
+                ''',
             },
             {
                 'name': 'The Lakewalk',
                 'link': 'https://www.google.com/maps/dir/46.7800223,-92.0913201/46.8025932,-92.0686212/@46.7913221,-92.090369,15z/am=t/data=!3m1!4b1!4m2!4m1!3e2?hl=en',
-                'description': 'Starting in Canal Park and snaking north up the shore, the lake walk is a gorgeous stretch of trail for walking or biking.',
+                'description': '''
+                    Starting in Canal Park and snaking north up the shore, the
+                    lake walk is a gorgeous stretch of trail for walking or
+                    biking.
+                ''',
             },
             {
                 'name': 'Aerial Lift Bridge',
                 'link': 'https://www.google.com/maps/place/Aerial+Lift+Bridge/@46.778977,-92.0950788,17z/data=!3m1!4b1!4m5!3m4!1s0x52ae52ec55a6c0ad:0xb7092797d58f931e!8m2!3d46.7789733!4d-92.0928901?hl=en',
-                'description': 'A Duluth landmark in Canal Park, the lift bridge features an uncommon design where the center span is lifted straight up to let barges pass. Sick.',
+                'description': '''
+                    A Duluth landmark in Canal Park, the lift bridge features
+                    an uncommon design where the center span is lifted straight
+                    up to let barges pass. Sick.
+                ''',
             },
             {
                 'name': 'Park Point beach',
                 'link': 'http://www.duluthmn.gov/parks/parks-listing/park-point/',
-                'description': u'If you feel like crossing the lift bridge, you can reward yourself with a trip to the beach. There’s parking, plenty of sand, and a beach house with bathrooms.',
+                'description': u'''
+                    If you feel like crossing the lift bridge, you can reward
+                    yourself with a trip to the beach. There’s parking, plenty
+                    of sand, and a beach house with bathrooms.
+                ''',
             },
             {
                 'name': 'The Depot',
                 'link': 'http://duluthdepot.org/',
-                'description': u'The old train terminal downtown is now a museum where you can learn about the area’s railroad history and look at old trains. We went and thought it was pretty fun.',
+                'description': u'''
+                    The old train terminal downtown is now a museum where you
+                    can learn about the area’s railroad history and look at old
+                    trains. We went and thought it was pretty fun.
+                ''',
             },
             {
                 'name': 'Skyline Drive and Enger Tower',
                 'link': 'http://www.superiortrails.com/duluth-engerpark.html',
-                'description': 'The Skyline Drive is a really pretty road along a ridgeline above Duluth, and Enger Tower is a nice stop off point to get a panoramic view of the city.',
+                'description': '''
+                    The Skyline Drive is a really pretty road along a ridgeline
+                    above Duluth, and Enger Tower is a nice stop off point to
+                    get a panoramic view of the city.
+                ''',
             },
             {
                 'name': u'“Cruisin’ the loop”',
                 'link': 'https://www.google.com/maps/dir/46.7803981,-92.092697/46.7804495,-92.0926112/@46.780743,-92.0935627,18z/data=!3m1!4b1!4m2!4m1!3e0?hl=en',
-                'description': u'Imagine you are a high school student. Your social life is limited; you can’t yet drink or afford to go out a lot, but you want to see and be seen. What do you do? You Cruise the Loop. Head down to Canal Park, open your windows, and crank the tunes that show your refined sensibilities, and make a pass around the loop at the end of the road.',
+                'description': u'''
+                    Imagine you are a high school student. Your social life is
+                    limited; you can’t yet drink or afford to go out a lot, but
+                    you want to see and be seen. What do you do? You Cruise the
+                    Loop. Head down to Canal Park, open your windows, crank the
+                    tunes that show your refined sensibilities, and make a pass
+                    around the loop at the end of the road.
+                ''',
             },
             {
                 'name': 'Duluth Family Sauna',
                 'link': 'https://www.yelp.com/biz/duluth-family-sauna-duluth',
-                'description': u'As far as we can tell, it is a sauna, and it is in Duluth, but it doesn’t seem family friendly.',
+                'description': u'''
+                    As far as we can tell, it is a sauna, and it is in Duluth,
+                    but it doesn’t seem family friendly.
+                ''',
             },
             {
                 'name': 'The Zinema',
                 'link': 'https://zeitgeistarts.com/zinema/',
-                'description': 'An independent 2-screen cinema that shows great films and serves beer.',
+                'description': '''
+                    An independent 2-screen cinema that shows great films and
+                    serves beer.
+                ''',
             },
             {
                 'name': 'The Electric Fetus',
                 'link': 'http://www.electricfetus.com/',
-                'description': 'A locally famous record- and gift-shop. Prince was in the Minneapolis branch on the day that he died.',
+                'description': '''
+                    A locally famous record- and gift-shop. Prince was in the
+                    Minneapolis branch on the day that he died.
+                ''',
             },
         ],
     },
@@ -65,17 +105,27 @@ STUFF_TO_DO = [
             {
                 'name': 'Gooseberry Falls',
                 'link': 'http://www.dnr.state.mn.us/state_parks/gooseberry_falls/index.html',
-                'description': 'A pretty waterfalls with a visitor center about 35 minutes north of Lake Breeze.',
+                'description': '''
+                    A pretty waterfalls with a visitor center about 35 minutes
+                    north of Lake Breeze.
+                ''',
             },
             {
                 'name': 'Split Rock Lighthouse',
                 'link': 'http://www.dnr.state.mn.us/state_parks/split_rock_lighthouse/index.html',
-                'description': 'A historic lighthouse / museum with beautiful grounds about 45 minutes north of Lake breeze.',
+                'description': '''
+                    A historic lighthouse / museum with beautiful grounds about
+                    45 minutes north of Lake Breeze.
+                ''',
             },
             {
                 'name': 'Temperance River State Park',
                 'link': 'http://www.dnr.state.mn.us/state_parks/temperance_river/index.html',
-                'description': 'A picturesque river gorge and trail system featuring waterfalls and cauldrons, 1 hour and 20 minutes north of Lake Breeze.',
+                'description': '''
+                    A picturesque river gorge and trail system featuring
+                    waterfalls and cauldrons, 1 hour and 20 minutes north of
+                    Lake Breeze.
+                ''',
             },
         ],
     },
@@ -87,52 +137,86 @@ STUFF_TO_DO = [
             {
                 'name': u'Pizza Lucé',
                 'link': 'https://pizzaluce.com/locations/duluth',
-                'description': u'Tasty pizza and a good local beer list right downtown. But there’s no easy way to say this: we are having pizza at the wedding, so try to not overdo it.',
+                'description': u'''
+                    Tasty pizza and a good local beer list right downtown. But
+                    there’s no easy way to say this: we are having pizza at the
+                    wedding, so try to not overdo it.
+                ''',
             },
             {
                 'name': 'New Scenic Cafe',
                 'link': 'http://newsceniccafe.com/',
-                'description': u'An excellent, cozy, and pretty much gourmet restaurant up the shore a ways. Try the Borealis beer if you’re drinking; the Scenic is one of the only places you can find this hyper-local Belgian style brand.',
+                'description': u'''
+                    An excellent, cozy, and pretty much gourmet restaurant up
+                    the shore a ways. Try the Borealis beer if you’re drinking;
+                    the Scenic is one of the only places you can find this
+                    hyper-local Belgian style brand.
+                ''',
             },
             {
                 'name': 'The Duluth Grill',
                 'link': 'https://duluthgrill.com/',
-                'description': 'Low key northlands fare in West Duluth; they grow produce in their parking lot.',
+                'description': '''
+                    Low key northlands fare in West Duluth; they grow produce
+                    in their parking lot.
+                ''',
             },
             {
                 'name': 'Chester Creek Cafe',
                 'link': 'http://astccc.net/',
-                'description': 'kinda hippie',
+                'description': '''
+                    kinda hippie
+                ''',
             },
             {
                 'name': 'Bent Paddle Brewing Co.',
                 'link': 'http://bentpaddlebrewing.com/',
-                'description': u'Our favorite local brewery, located in West Duluth. We’ve heard that you can order food from a nearby barbeque place to the taproom.',
+                'description': u'''
+                    Our favorite local brewery, located in West Duluth. We’ve
+                    heard that you can order food from a nearby barbeque place
+                    to the taproom.
+                ''',
             },
             {
                 'name': u'Fitger’s Brewhouse',
                 'link': 'https://fitgersbrewhouse.com/',
-                'description': 'Yummy beer; look out for barleywine on tap.',
+                'description': '''
+                    Yummy beer; look out for barleywine on tap.
+                ''',
             },
             {
                 'name': u'Sir Ben’s',
                 'link': 'https://www.sirbens.com/',
-                'description': 'Catch some politely quiet bluegrass or some exceptionally shitty karaoke at this unassuming bar.',
+                'description': '''
+                    Catch some politely quiet bluegrass or some exceptionally
+                    shitty karaoke at this unassuming bar.
+                ''',
             },
             {
                 'name': 'Canal Park Brewing Co.',
                 'link': 'https://canalparkbrewery.com/',
-                'description': 'A decent brewery with a great location in Canal Park. We usually sit outside and have a beer and truffle fries while we look at the lake.',
+                'description': '''
+                    A decent brewery with a great location in Canal Park. We
+                    usually sit outside and have a beer and truffle fries while
+                    we look at the lake.
+                ''',
             },
             {
                 'name': 'Vikre Distillery',
                 'link': 'http://www.vikredistillery.com/',
-                'description': 'A whiskey, gin, and aquavit distillery in Canal Park with a nice cocktail list.',
+                'description': '''
+                    A whiskey, gin, and aquavit distillery in Canal Park with a
+                    nice cocktail list.
+                ''',
             },
             {
                 'name': u'Matilda’s Dog Bakery',
                 'link': 'http://www.matildasdogbakery.com/',
-                'description': u'Pick up something for your loved ones back home from this local, home-made dog treat shop. Hot tip: get Al’s Chicago Pizza if it’s in stock; it’s a rare find.',
+                'description': u'''
+                    Pick up something for your loved ones back home from this
+                    local, home-made dog treat shop. Hot tip: get Al’s Chicago
+                    Pizza if it’s in stock; it’s a rare find.
+                ''',
             }
         ],
     },
@@ -144,17 +228,37 @@ STUFF_TO_DO = [
             {
                 'name': u'The Boundary Waters Canoe Area',
                 'link': 'http://www.dnr.state.mn.us/watertrails/bwca/index.html',
-                'description': 'The Boundary Waters is a huge network of interconnected lakes in the border area between Minnesota and Canada. Rent a canoe (try Sawtooth Outfitters in Tofte) and do anything from an overnight to a multi-day trip. We\'ve enjoyed an overnight to Larch Lake, and a two-night trip to Crystal Lake.',
+                'description': u'''
+                    The Boundary Waters is a huge network of interconnected
+                    lakes in the border area between Minnesota and Canada. Rent
+                    a canoe (try Sawtooth Outfitters in Tofte) and do anything
+                    from an overnight to a multi-day trip. We’ve enjoyed an
+                    overnight to Larch Lake, and a two-night trip to Crystal
+                    Lake.
+                ''',
             },
             {
                 'name': u'The Superior Hiking Trail',
                 'link': 'https://shta.org/',
-                'description': u'The SHT stretches over 300 miles from Duluth along the shore to Canada. You can do an easy and really pretty overnight to Bear Lake (start from a parking area on Route 11, outside of Silver Bay), or through-hike for a longer section of trail — drop one car at the end or take the shuttle back.',
+                'description': u'''
+                    The SHT stretches over 300 miles from Duluth along the
+                    shore to Canada. You can do an easy and really pretty
+                    overnight to Bear Lake (start from a parking area on Route
+                    11, outside of Silver Bay), or through-hike for a longer
+                    section of trail — drop one car at the end or take the
+                    shuttle back.
+                ''',
             },
             {
                 'name': u'Climbing at Palisade Head',
                 'link': 'https://www.mountainproject.com/area/105814857/palisade-head',
-                'description': u'This bluff not in Tettegouche State Park has some of Minnesota\'s best climbing. It\'s a scenic cliff of volcanic rock over Lake Superior where you can build an anchor, lower down to the water, and climb out.',
+                'description': u'''
+                    This bluff in Tettegouche State Park has some of
+                    Minnesota’s best climbing. It’s a scenic cliff of volcanic
+                    rock over Lake Superior where you can build an anchor,
+                    lower down to the water, and climb out. Also, it’s where
+                    the pivotal final scene of the movie The Good Son was shot.
+                ''',
             },
         ],
     },
