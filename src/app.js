8546a15a1a9e10ffacb5c2e99c62c44b2dc8388b
Michael P. Geraci <me@mgeraci.com>
2018-03-12 22:21:49 -0400

    add a title text-shadow animation on the home page

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import "babel-polyfill";
 import BackgroundChanger from "./background_changer";
 import LemonParty from "./lemon_party";
+import HomeTitleAnimator from "./pages/home.js"
 
 import "./app.scss";
 
 BackgroundChanger.init();
 LemonParty.init();
+HomeTitleAnimator.init();
